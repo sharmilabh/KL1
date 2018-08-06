7a304bfb4f87c9d7ba98301dc29fd6b84428be03
Sharmila <sharmila.bhardwaj@eventsforce.com>
2018-08-06 12:48:19 +0100

    Update 3

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
+c:
+cd C:\Automation\Katalon_Studio_Windows_64-5.5
 katalon -noSplash  -runMode=console -consoleLog -projectPath="D:\Jenkins\KatalonAutomationTest\workspace\KL1.prj" -retry=0 -testSuitePath="Test Suites/TestSuiteKL" -executionProfile="default" -browserType="Chrome"
