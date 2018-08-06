4721fe3542860334eb2be9aa4d2a0ab0239bfcd4
Sharmila <sharmila.bhardwaj@eventsforce.com>
2018-08-06 12:16:40 +0100

    Update some more

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-C:\Automation\Katalon_Studio_Windows_64-5.5\katalon.exe -runMode=console -projectPath="*\master\KL1.prj" -retry=0 -testSuitePath="Test Suites/TestSuiteKL" -browserType="Chrome"
+C:\Automation\Katalon_Studio_Windows_64-5.5\katalon.exe -runMode=console -projectPath="KL1.prj" -retry=0 -testSuitePath="Test Suites/TestSuiteKL" -browserType="Chrome"
